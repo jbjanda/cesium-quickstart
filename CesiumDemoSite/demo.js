48e66517a27f7a361077d737502df7357179c21d
jbjanda <jbjanda@gmail.com>
2016-04-19 17:11:02 +0000

    coding script and czml inline

--- CesiumDemoSite/demo.js
+++ CesiumDemoSite/demo.js
@@ -1,40 +1,47 @@
+Cesium.Viewer('cesiumContainer');
+var button = document.getElementById("startDemo");
+if (button){
 document.getElementById("startDemo").addEventListener("click", startViewer);
+}
 
 
 function startViewer(){
-var czml = [
-  {
-    "id" : "document",
-    "name" : "CZML Point",
-    "version" : "1.0"
-  },
-  {
-    "id" : "point 1",
-    "name": "point",
-    "position" : {
-      "cartographicDegrees" : [-79.6509,	30.3729, 0]
+  var czml = [
+    {
+      "id" : "document",
+      "name" : "CZML Point",
+      "version" : "1.0"
     },
-    "point": {
-      "color": {
-        "rgba": [255,0, 200, 255]
+    {
+      "id" : "point 1",
+      "name": "point",
+      "position" : {
+        "cartographicDegrees" : [-79.6509,	30.3729, 0]
       },
-      "outlineColor": {
-        "rgba": [255,0, 200, 255]
-      },
-      "pixelSize": {
-        "number": 10
+      "point": {
+        "color": {
+          "rgba": [255,0, 0, 255]
+        },
+        "outlineColor": {
+          "rgba": [255,0, 200, 255]
+        },
+        "pixelSize": {
+          "number": 10
+        }
       }
     }
+  ];
+
+ var viewer = document.getElementById("cesiumContainer")
+  if (viewer){
+  var dataSource = Cesium.CzmlDataSource.load(czml);
+  viewer.dataSources.add(dataSource);
+  viewer.zoomTo(dataSource);
+  document.getElementById("cesiumContainer")=viewer;
   }
-];
-
-
-var viewer = new Cesium.Viewer('cesiumContainer');
-var dataSource = Cesium.CzmlDataSource.load(czml);
-viewer.dataSources.add(dataSource);
-viewer.zoomTo(dataSource);
-  document.getElementById("cesiumContainer")= viewer;
-  }
-
-
-
+/*
+  var dataSource = Cesium.CzmlDataSource.load(czml);
+  viewer.dataSources.add(dataSource);
+  viewer.zoomTo(dataSource);
+  document.getElementById("cesiumContainer")= viewer;*/
+}
