08df11dbd75144418483d4103585c99fdfa46097
jbjanda <jbjanda@gmail.com>
2016-04-20 14:03:54 +0000

    web site not looking correctly

--- CesiumDemoSite/demo.js
+++ CesiumDemoSite/demo.js
@@ -39,9 +39,53 @@ function startViewer(){
   viewer.zoomTo(dataSource);
   document.getElementById("cesiumContainer")=viewer;
   }
+
+
+
+
+
 /*
+
+Code below works inline!---------------------------------------------------
+
+var viewer= new Cesium.Viewer('cesiumContainer');
+
+
+document.getElementById("startDemo").addEventListener("click", startViewer);
+
+
+
+function startViewer(){
+  var czml = [
+    {
+      "id" : "document",
+      "name" : "CZML Point",
+      "version" : "1.0"
+    },
+    {
+      "id" : "point 1",
+      "name": "point",
+      "position" : {
+        "cartographicDegrees" : [-79.6509,	30.3729, 0]
+      },
+      "point": {
+        "color": {
+          "rgba": [255,0, 0, 255]
+        },
+        "outlineColor": {
+          "rgba": [255,0, 200, 255]
+        },
+        "pixelSize": {
+          "number": 10
+        }
+      }
+    }
+  ];
+
+
+
   var dataSource = Cesium.CzmlDataSource.load(czml);
   viewer.dataSources.add(dataSource);
   viewer.zoomTo(dataSource);
-  document.getElementById("cesiumContainer")= viewer;*/
+  document.getElementById("cesiumContainer")=viewer;*/
 }
