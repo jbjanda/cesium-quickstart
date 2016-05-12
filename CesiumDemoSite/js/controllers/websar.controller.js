7a9049382ae34de741b783c7cf8ed72259dd9479
jbjanda <jbjanda@gmail.com>
2016-05-12 13:33:01 +0000

    web form is runnning

--- CesiumDemoSite/js/controllers/websar.controller.js
+++ CesiumDemoSite/js/controllers/websar.controller.js
@@ -2,20 +2,20 @@
   angular.module('webForm')
         .controller('WebsarController', WebsarController);
 
-  WebsarController.$inject = ['$scope', '$http','ObjectService', 'MapService']
+  WebsarController.$inject = ['$scope', '$http','ObjectService']
 
-  function WebsarController($scope, $http, ObjectService, MapService) {
+  function WebsarController($scope, $http, ObjectService) {
     //Object structure to store all categories of objects with the associated flags, icons, and values.
     //Values are coded with id numbers and name/description
     $scope.objects = ObjectService.objects;
-    $scope.map = MapService.getMap();
-    $scope.user = MapService.user;
-
+//   $scope.map = MapService.getMap();
+//     $scope.user = MapService.user;
+      $scope.user = {};
     //Watches for changes in form values for longitudue and re-centers map
-    $scope.$watch('user.latlong', function(oldValue, newValue) {
-        console.log($scope.user)
-        $scope.map.getView().setCenter(ol.proj.fromLonLat([$scope.user.latlong.lon, $scope.user.latlong.lat]));
-      });
+//     $scope.$watch('user.latlong', function(oldValue, newValue) {
+//         console.log($scope.user)
+//         $scope.map.getView().setCenter(ol.proj.fromLonLat([$scope.user.latlong.lon, $scope.user.latlong.lat]));
+//       });
 
     // //Watches for changes in form values for latitude and re-centers map
     // $scope.$watch('user.latlong.lat', function(oldValue, newValue) {
@@ -38,43 +38,44 @@
     };
 
     //Toggle function to turn off other collapsable divs if the a new button is clicked and resets object input.
-    //Extra check to make sure the object is reset if a different button is clicked.
-    $scope.toggle = function (object) {
-      $scope.objects.map(function(item){
-        if(item === object){
-          if(item.isCollapsed){
-            item.isCollapsed = false;
-            $scope.user.objectID = '';
-          } else {
-            item.isCollapsed = true;
-            angular.forEach(item.values, function(val){
-              val.isHidden = true;
-            });
-          }
-        }
-      })
+   //Extra check to make sure the object is reset if a different button is clicked.
+   $scope.toggle = function (object) {
+     $scope.objects.map(function(item){
+       item.isCollapsed = true;
+       item.values.forEach(function(val){
+         val.isHidden = true;
+       })
+     });
+     var objs = $scope.objects.filter(function(o){
+       return o === object;
+     });
+     objs.map(function(obj){
+       obj.isCollapsed = false;
+       $scope.user.objectID = '';
+     });
 
-    };
+   };
 
 
-    //Toggle function to expand and collapse sub radio button menus if they exisit.
-    $scope.toggle2 = function (items) {
-      $scope.objects.map(function(obj){
-        obj.values.map(function(val){
-          if(val === items && val.sub){
-            if(val.isHidden){
-              val.isHidden = false;
-            }
-          } else {
-            val.isHidden = true;
-          }
-        });
 
-      if($scope.user.objectID.slice(0,2) === '-1'){
-        $scope.user.objectID = '';
-      }
-    });
-  };
+
+   //Toggle function to expand and collapse sub radio button menus if they exisit.
+   $scope.toggle2 = function (items) {
+     $scope.objects.map(function(obj){
+       obj.values.map(function(val){
+         if(val === items && val.sub){
+           if(val.isHidden){
+             val.isHidden = false;
+           }
+         } else {
+           val.isHidden = true;
+         }
+       });
+
+    
+       
+   });
+ };
 
 
 
