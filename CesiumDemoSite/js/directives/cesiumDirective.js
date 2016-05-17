df504d3046274d452477c2bee0b42c5eda021abd
jbjanda <jbjanda@gmail.com>
2016-05-17 15:31:40 +0000

    very basic angular deployment with cesium

---
+++ CesiumDemoSite/js/directives/cesiumDirective.js
@@ -0,0 +1,41 @@
+app.directive('cesiumDirective', function($interval){
+    return {
+      restrict: "EA",
+      controllerAs: "cesiumCtrl",
+      scope: {
+        data: "="
+      },
+      controller: "appCtrl",
+      link: function(scope, element, attr, ctrl){
+
+        ctrl.cesium = new Cesium.Viewer(element[0], {
+          baseLayerPicker: false,
+          fullscreenButton: false,
+          homeButton: false,
+          sceneModePicker: false,
+          selectionIndicator: false,
+          timeline: false,
+          animation: false,
+          geocoder: false
+        });
+
+
+        //Grand Island, Nebraska in radians
+//         var startingLocation = {
+//             centerLong: (-98.343286 * Math.PI / 180),
+//             centerLat: (40.923664 * Math.PI / 180)
+//         };
+
+//         ctrl.cesium.camera.flyTo({
+//             destination: new Cesium.Cartesian3.fromDegrees(startingLocation.centerLong * 180/Math.PI, startingLocation.centerLat * 180/Math.PI, 5000000)
+//         });
+
+//         ctrl.cesium.entities.add({
+//           position: new Cesium.Cartesian3.fromDegrees(startingLocation.centerLong * 180/Math.PI, startingLocation.centerLat * 180/Math.PI, 500),
+//           point: {
+//             pixelSize: 32
+//           }
+//         });
+      }
+    };
+  });
