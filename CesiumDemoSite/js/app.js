e955add8772e25b3598a87117f9788ba794a1707
jbjanda <jbjanda@gmail.com>
2016-05-27 13:53:39 +0000

    final with comments try two

--- CesiumDemoSite/js/app.js
+++ CesiumDemoSite/js/app.js
@@ -1,4 +1,18 @@
-// define the ngCesium module
+/*
+Programmer: Jeff Janda
+Date: 5/27/16
+
+This module allows for the creation of a cesium viewer. This app is not as independent as it should be
+and functions will require the form used in the deployment to work. At this time this will allow
+a Cesium viewer to appear on the page, allow a user to select a lat and lon from the viewer and
+put the values into the form and then run a demonstration. Code can accept a czml variable
+but at this point the czml is hard coded into the code. Thanks for giving me a chance to work
+on this it was fun!
+
+*/
+
+
+
 angular.module('ngCesium', [])
     //define the cesium directive
     .directive('cesiumDirective', function($interval, cesiumService){
@@ -11,8 +25,13 @@ angular.module('ngCesium', [])
       controller: function($scope, $element){
 
       },
-      // define the "link" function
+      /*
+      All functionality was programmed into the Link function. Most of this should probably be
+      loaded into a service.
+      */
       link: function(scope, element, attr, ctrl){
+
+        // Creates a viewer and sets optional properities
         ctrl.cesiumViewer = new Cesium.Viewer(element[0], {
               baseLayerPicker: false,
               fullscreenButton: false,
@@ -24,28 +43,17 @@ angular.module('ngCesium', [])
               geocoder: false
             });
 
+        // this variable allows the lat and lon to be displayed on the viewer
         var entity = ctrl.cesiumViewer.entities.add({
         label : {
             show : false
         }
         });
 
-        // Start of Picker function Note: longitudeString and latitudeString used in click event function below.
-        var cartesian;
-        var cartographic;
-        var longitudeString; // value of the longitude displayed in the viewer
-        var latitudeString;  // value of the latitude displayed in the viewer
 
-        var scene = ctrl.cesiumViewer.scene;
-       // console.log(scene);
-        var cam=ctrl.cesiumViewer.camera;
-       // console.log(cam);
-        var demoStarted=false; // checks if the demo is started
-
-        // Toggles the form open and closed
-
+          // Toggles the form open and closed
          scope.toggleForm = function() {
-           element.toggleClass('withForm');
+           element.toggleClass('withForm'); // withForm class styles the viewer to be smaller
            if(scope.showForm) {
                scope.showForm = false;
 
@@ -55,9 +63,24 @@ angular.module('ngCesium', [])
             }
           }
 
+        // These variables are part of the picker code below.
+        //Note: longitudeString and latitudeString used in click event function below.
+        var cartesian;
+        var cartographic;
+        var longitudeString; // value of the longitude displayed in the viewer
+        var latitudeString;  // value of the latitude displayed in the viewer
+
+        var scene = ctrl.cesiumViewer.scene;
+
+        var cam=ctrl.cesiumViewer.camera;
+
+        var demoStarted=false; // checks if the demo is started and used to turn on and off the lat/lon display
+
+
+
 
     // Mouse over the globe to see the cartographic position
-    // This function is taken from the code examples in the Cesium Sandcastle under Picker
+    // This function is taken from the code examples in the Cesium Sandcastle under Picker and adapted for our app
     handlerMove = new Cesium.ScreenSpaceEventHandler(scene.canvas);
     handlerMove.setInputAction(function(movement) {
         cartesian = cam.pickEllipsoid(movement.endPosition, scene.globe.ellipsoid);
@@ -91,11 +114,13 @@ angular.module('ngCesium', [])
 
 
         // START DEMO Code Below--------------------------------------------------->
+        // The init function will take in a CZML and start it running in the viewer
         scope.init = function(){
 
 
         // START CZML CODE----------------------------------------------------------------->
  // This is the formatted websar data. All data is hardcoded in at this time. Used in description for CZML
+ // This displays when any time point is clicked in the viewer
       var websar = "<p>CONDITIONS AT MAP CENTER 35-34.0N 073-46.0N VALID ON 2412900Z NOV2015</P>" +
       "<TABLE style=\"font-size:7pt\">" +
 
@@ -174,18 +199,23 @@ angular.module('ngCesium', [])
 
       //Function Start
 
+/*
+This CZML controls a 24 hour drift cycle. We used a static start time using the availability property
+and used the interval property of each point to control color changing. This also draws a line that
+will connect the points as they appear on the map. We gave the line a 10 second lead time and a trail time
+of 24 hours.
+*/
 
-       // viewer = new Cesium.Viewer('cesiumContainer');
               var czml = [
                 {
                   "id" : "document",
                   "name" : "CZML Point",
                   "version" : "1.0"
                 },
+                // Polyline code below.
                 {
                   "id" : "path",
                   "name" : "Object Float Path",
-
                   "availability" : "2016-04-20T00:00:00Z/2016-04-21T00:00:00Z",
                   "path" : {
                     "material" : {
@@ -232,6 +262,9 @@ angular.module('ngCesium', [])
                     ]
                   }
                 },
+
+                // Points code below will comment the first few points but all others
+                // will be the same accept for the time interval.
                 {
                   "id" : "point 1",
                   "name": "2016-04-20T00:00:00Z",
@@ -244,7 +277,7 @@ angular.module('ngCesium', [])
                   },
                   "point": {
                     "color": {
-                      "rgba": [255,0, 0, 255]
+                      "rgba": [255,0, 0, 255] // red
                     },
                     "outlineColor": {
                       "rgba": [255,0, 200, 255]
@@ -268,11 +301,11 @@ angular.module('ngCesium', [])
                     "color": [
                     {
                       "interval": "2016-04-20T01:00:00Z/2016-04-20T02:00:00Z",
-                      "rgba": [0,0, 255, 255]
+                      "rgba": [0,0, 255, 255] // blue
                     },
                     {
                       "interval" : "2016-04-20T02:00:00Z/9999-12-31T24:00:00Z",
-                        "rgba": [128, 128, 128, 255]
+                        "rgba": [128, 128, 128, 255] // gray
                     }
                     ],
                     "outlineColor": {
@@ -900,13 +933,14 @@ angular.module('ngCesium', [])
                 }
                 ]; // end czml
 // END CZML CODE-------------------------------------------------------------------->
-     var dataSource = Cesium.CzmlDataSource.load(czml);
-        ctrl.cesiumViewer.dataSources.add(dataSource);
-        var frame= new Cesium.HeadingPitchRange(0.05,-.30, 200500);
+
+        var dataSource = Cesium.CzmlDataSource.load(czml); // creates the datasource
+        ctrl.cesiumViewer.dataSources.add(dataSource); // adds datasource to the viewer
+        var frame= new Cesium.HeadingPitchRange(0.05,-.30, 200500); // orients the camera for the zoomTo
         demoStarted=true; // This removes the cartographic degree label from the viewer when the demo starts.
-        ctrl.cesiumViewer.zoomTo(dataSource, frame);
-        element.toggleClass('withForm');
-        scope.showForm = false;
+        ctrl.cesiumViewer.zoomTo(dataSource, frame); // zooms to the datasource with the camera oriented
+        element.toggleClass('withForm'); // makes the viewer full size
+        scope.showForm = false; // closes the form
         };// end init function
             }// end link function
           };
@@ -914,7 +948,7 @@ angular.module('ngCesium', [])
 
         })
 
-    // define the cesium factory
+   // this displays default values into the form
     .factory('cesiumService', function(){
          var service = {};
          service.user = {
